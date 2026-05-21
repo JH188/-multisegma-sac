5979eb20d8bdcaa5a5d75df200dab45f35862ff7
Jonathan Huaman <jonathan.huaman18@gmail.com>
2026-05-21 14:38:03 -0500

    Permitir CORS frontend Railway

--- backend/src/main/java/com/lisume/backend/config/CorsConfig.java
+++ backend/src/main/java/com/lisume/backend/config/CorsConfig.java
@@ -7,15 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
   @Bean
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4201")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                .allowCredentials(false);
+            .allowedOrigins(
+                "http://localhost:4201",
+                "http://localhost:4200",
+                "https://desirable-motivation-production-a993.up.railway.app"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false);
       }
     };
   }
